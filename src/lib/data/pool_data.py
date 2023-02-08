69236b3bf534c7e2af69aec9fe1b0cee19df6d93
Wael <wael.t.ivie@gmail.com>
2023-02-08 10:08:18 -0800

    first commit

--- src/lib/data/pool_data.py
+++ src/lib/data/pool_data.py
@@ -45,6 +45,7 @@ block_count = int(piada_info.block_count[0])
 epoch = int(tip.epoch_no[0])
 delegate_rewards = round((piada_history.deleg_rewards.astype(int) / 1000000).sum())
 ten_epoch_ros = round(piada_history.epoch_ros[0:9].mean(), 2)
+life_time_ros = round(piada_history.epoch_ros.mean(), 2)
 
 # lets make the some of the numbers look nice and convert to strings to look like this "80,000" without the ending .0
 pledge = "{:,}".format(pledge)
@@ -52,7 +53,9 @@ delegate_rewards = "{:,}".format(delegate_rewards)
 total_delegated = "{:,}".format(total_delegated)
 
 # now lets get all this data above into a dictionary with keys as variable names and values as the variable values
-pool_data = dict(pledge=pledge, total_delegated=total_delegated, number_of_delegators=number_of_delegators, pool_fee=pool_fee, block_count=block_count, epoch=epoch, delegate_rewards=delegate_rewards, ten_epoch_ros=ten_epoch_ros)
+pool_data = dict(pledge=pledge, total_delegated=total_delegated, number_of_delegators=number_of_delegators, 
+                pool_fee=pool_fee, block_count=block_count, epoch=epoch, delegate_rewards=delegate_rewards, 
+                ten_epoch_ros=ten_epoch_ros, life_time_ros=life_time_ros)
 
 # now lets convert the dictionary to a json file
 pool_data_json = json.dumps(pool_data)
